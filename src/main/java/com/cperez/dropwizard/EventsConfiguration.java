76194ab8259a3095351cabb612402bc1308f4b75
Cesar Daniel Perez <cesdperez@gmail.com>
2016-12-17 18:57:18 -0300

    Configuration changes
    Added dateFormat parameter and validated it's not empty.

--- src/main/java/com/cperez/dropwizard/EventsConfiguration.java
+++ src/main/java/com/cperez/dropwizard/EventsConfiguration.java
@@ -1,10 +1,14 @@
 package com.cperez.dropwizard;
 
 import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class EventsConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    @NotEmpty
+    private String dateFormat;
+
+    public String getDateFormat() {
+        return dateFormat;
+    }
 }
